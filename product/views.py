8330e379b31261fbd66239edad00be3028cfdb2f
Ataiskih <Ataiskih@users.noreply.github.com>
2020-07-13 14:58:56 +0600

    added alerts to edit and create product

--- product/views.py
+++ product/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from product.models import Product, Category
 from product.forms import ProductForm
-from django.db.models import Q 
+from django.db.models import Q
 
 
 def products(request):
     if "key_word" in request.GET:
         key = request.GET.get("key_word")
-        products = Product.objects.filter(Q(availability_in_store=True),
+        products = Product.objects.filter(
+            Q(availability_in_store=True),
             Q(name__icontains=key) |
             Q(description__icontains=key) |
             Q(category__name__contains=key)
@@ -48,7 +49,7 @@ def product_create(request):
         form = ProductForm()
         message = "Добавить товар"
         return render(
-            request, 
+            request,
             "product/form.html",
             {"form": form, "message": message}
         )
@@ -64,8 +65,10 @@ def product_edit(request, id):
         )
         if form.is_valid():
             form.save()
-            return redirect(
-                "product", id=product.id
+            alert = "Вы изменили публикацию"
+            return render(
+                request, "product/product.html",
+                {'alert': alert, 'product': product}
             )
     elif request.method == "GET":
         product = Product.objects.get(id=id)
