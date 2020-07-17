19b776adaf7c9ae6f7a43211a66f63b61e6ce786
Ataiskih <Ataiskih@users.noreply.github.com>
2020-07-18 01:48:49 +0600

    added profile page for saler on top header

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from core.views import home
+from core.views import home, profile
 
 
 urlpatterns = [
     path("", home, name="home"),
+    path("profile/", profile, name="profile"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from product.models import Product
 
 
 def home(request):
     return redirect("products")
+
+
+@login_required(login_url="/login/")
+def profile(request):
+    products = Product.objects.filter(
+        availability_in_store=True,
+        user=request.user
+    )
+    return render(
+        request, 
+        "core/profile.html",
+        {"products": products}
+    )
