c932728017afcd40a679000c539b6721badfd34d
Ataiskih <Ataiskih@users.noreply.github.com>
2020-07-25 02:06:17 +0600

    with bags in password, email in edit profile

--- core/forms.py
+++ core/forms.py
@@ -4,14 +4,34 @@ from django import forms
 from django.contrib.auth.models import User
 
 
-class MyCustomChangePasswordForm(ChangePasswordForm):
-    oldpassword = PasswordField()
-    password1 = SetPasswordField()
-    password2 = PasswordField()
+class MyCustomChangePasswordForm(ChangePasswordForm, forms.ModelForm):
+
+    oldpassword = PasswordField(label= ("Current Password"))
+    password1 = SetPasswordField(label= ("New Password"))
+    password2 = PasswordField(label= ("New Password (again)"))
+
+    class Meta:
+        model = User
+        fields = ["oldpassword", "password1", "password2"]
+
+    def __init__(self, *args, **kwargs):
+        super(ChangePasswordForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].user = self.user
+
+    def clean_oldpassword(self):
+        if not self.user.check_password(self.cleaned_data.get("oldpassword")):
+            raise forms.ValidationError(_("Please type your current"
+                                          " password."))
+        return self.cleaned_data["oldpassword"]
+
+    def save(self):
+        get_adapter().set_password(self.user, self.cleaned_data["password1"])
+
 
 class ChangePersonalInfo(forms.ModelForm):
+
+    
     class Meta:
         model = User
         fields = ["username", "first_name", "last_name"]
-
 
--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from product.models import Product
 from core.forms import MyCustomChangePasswordForm, \
     ChangePersonalInfo
-    
+
 
 
 def home(request):
@@ -29,15 +29,22 @@ def profile_edit(request,id):
         if request.user != user:
             return redirect(home)
         else:
-            user = request.user
-            password_1 = request.POST.get("password1")
-            password_2 = request.POST.get("password2")
-            if password_1 == password_2:
-                user.set_password(password_1)
-                user.save()
-            return HttpResponseRedirect(
-                request.path_info
+            form = MyCustomChangePasswordForm(
+                request.POST
             )
+            if form.is_valid():
+                form.save()
+            # password_1 = request.POST.get("password1")
+            # password_2 = request.POST.get("password2")
+            # form = MyCustomChangePasswordForm(
+            #     request.POST
+            # )
+            # if password_1 == password_2:
+            #     user.set_password(password_1)
+            #     user.save()
+                return HttpResponseRedirect(
+                    request.path_info
+                )
     elif request.method == "POST" and "edit_profile" in request.POST:
         if request.user != user:
             return redirect(home)
@@ -47,6 +54,20 @@ def profile_edit(request,id):
                 instance=user)
             if form.is_valid():
                 form.save()
+                return HttpResponseRedirect(
+                    request.path_info
+                )
+    elif request.method == "POST" and "change_email" in request.POST:
+        if request.user != user:
+            return redirect(home)
+        else:
+            user = request.user
+            old_email = request.POST.get("old_email")
+            new_email = request.POST.get("new_email")
+            if user.email == old_email:
+                new_email = user.email
+                user.save()
+                print(user.email)
                 return HttpResponseRedirect(
                     request.path_info
                 )
