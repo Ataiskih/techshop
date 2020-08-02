62c02f3ca836790c3fc5a3cb16088f5d570e0388
Ataiskih <Ataiskih@users.noreply.github.com>
2020-08-03 03:23:20 +0600

    fix bags in POST GET in update product

--- product/views/product_product_edit.py
+++ product/views/product_product_edit.py
@@ -5,22 +5,26 @@ from product.forms.product_product import ProductForm
 from product.models.product_images import Images
 from product.forms.product_images import ImageForm
 from core.views.core_home import home
-from django.forms import modelformset_factory
+from django.forms import inlineformset_factory
 
 
 @login_required(login_url="/login/")
 def product_edit(request, id):
+    product = Product.objects.get(id=id)
+    images = Images.objects.filter(post=product)
+    ImageFormSet = inlineformset_factory(
+        Product, Images, fields=("image",))
     if request.method == "POST":
-        product = Product.objects.get(id=id)
-        images = Images.objects.filter(post=product)
-        ImageFormSet = modelformset_factory(
-            Images, form=ImageForm)
-        if request.user != product.user:
-            return redirect(home)
-        else:
-            form = ProductForm(request.POST, request.FILES, instance=product)
-            formset = ImageFormSet(data=request.POST, files=request.FILES, instance=product)
-            if form.is_valid() and formset.is_valid():
+        if request.user == product.user:
+            form = ProductForm(
+                request.POST, 
+                request.FILES, 
+                instance=product
+            )
+            formset = ImageFormSet(
+                request.POST, request.FILES, 
+                instance=product)
+            if formset.is_valid() and form.is_valid():
                 form.save()
                 formset.save()
                 alert = "Вы изменили публикацию"
@@ -29,20 +33,18 @@ def product_edit(request, id):
                     {'alert': alert, 'product': product,
                     "images": images}
                 )
-    elif request.method == "GET":
-        product = Product.objects.get(id=id)
-        images = Images.objects.filter(post=product)
-        ImageFormSet = modelformset_factory(
-            Images, form=ImageForm)
-        if request.user != product.user:
-            return redirect(home)
         else:
+            return redirect(home)
+    else:
+        if request.user == product.user:
+            formset = ImageFormSet(instance=product)
             form = ProductForm(instance=product)
-            formset = ImageFormSet(queryset=images)
             message = "Редактировать публикацию"
-            return render(
-                request, "product/form.html",
-                {"form": form, 
-                "formset": formset,
-                "message": message}
-            )
+        else:
+            return redirect(home)
+    return render(
+        request, "product/form.html",
+        {"form": form, 
+        "formset": formset,
+        "message": message}
+    )
